8db28a4818f913218129a48a947da46cac3951e3
dcdrawk <dcdrawk@gmail.com>
2014-03-09 21:14:12 -0700

    -fixed lightbox, moved images into a new subfolder called 'img' -made many changes to product_tie_interceptor.html -most of the changes updated the look -added a rating system, but need to add a place to leave a review -added a specifications and reviews button that toggle their respective content using ui.js

---
+++ _work/js/ui.js
@@ -0,0 +1,30 @@
+$(document).ready( function() {
+
+	/* 
+		Here is some good jQuery code for debugging, takes
+		whatever info you want and injects it into the title
+	*/
+	// var variable = "Debugging test";
+	// $("title").html("Info: "+variable);
+
+	/*
+	$("a[href$='.html']").click( function(clickEvent) {
+		
+		clickEvent.preventDefault();
+		
+		var file = $(this).attr("href");
+		// $("title").html("Info: "+file);
+		
+		$("section#main").load(file+" #content");
+	});
+	*/
+	$("#vesselSpecs").hide();
+	$("#reviews").hide();
+	$("#toggleSpecs").click(function(){
+		$("#vesselSpecs").slideToggle();
+	});
+	$("#toggleReviews").click(function(){
+		$("#reviews").slideToggle();
+	});
+	
+});
